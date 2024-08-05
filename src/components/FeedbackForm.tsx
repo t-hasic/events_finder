a5c1f937a70a01815b1ed5517e178bf6c7bc8ad3
t-hasic <thasic@mit.edu>
2024-08-05 14:24:26 -0400

    added loading & feedback form

---
+++ src/components/FeedbackForm.tsx
@@ -0,0 +1,25 @@
+function FeedbackForm() {
+  return (
+    <>
+      <div className="form-floating mb-3" style={{ width: "50%" }}>
+        <input
+          type="email"
+          className="form-control"
+          id="floatingInput"
+          placeholder="name@example.com"
+        />
+        <label htmlFor="floatingInput">Email address</label>
+      </div>
+      <div className="form-floating" style={{ width: "50%" }}>
+        <textarea
+          className="form-control"
+          placeholder="Leave a comment here"
+          id="floatingTextarea"
+        ></textarea>
+        <label htmlFor="floatingTextarea">Comments</label>
+      </div>
+    </>
+  );
+}
+
+export default FeedbackForm;
--- src/pages/FeatureSearch.tsx
+++ src/pages/FeatureSearch.tsx
@@ -2,10 +2,11 @@ import { useEffect } from "react";
 import React, { useState } from "react";
 
 import "../styles/App.css";
-import FilterButton from "../components/FilterButton";
 import Input from "../components/Input";
 import Loading from "../components/Loading";
 import Results from "../components/Results";
+import FeedbackForm from "../components/FeedbackForm";
+import FilterButton from "../components/FilterButton";
 
 import axios from "axios";
 
@@ -16,7 +17,8 @@ interface Filter {
 }
 
 function FeatureSearch() {
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [giveFeedback, setGiveFeedback] = useState(false);
   const [inputValue, setInputValue] = useState("");
   const [dateValue, setDateValue] = useState(
     new Date().toISOString().split("T")[0]
@@ -36,6 +38,24 @@ function FeatureSearch() {
           isActive: false,
         }));
         setFilters(filtersData);
+        const payload = {
+          query: "event",
+          start_date: dateValue,
+          start_time: timeValue,
+          tag_ids: [],
+        };
+        console.log("Payload: ", payload);
+        try {
+          const response = await axios.post(
+            "http://ec2-54-90-82-170.compute-1.amazonaws.com:9000/v1/events/search",
+            payload
+          );
+          console.log(response.data.activities);
+          setActivities(response.data.activities);
+          setIsLoading(false);
+        } catch (error) {
+          console.error("Error fetching data: ", error);
+        }
       } catch (error) {
         console.error("Error fetching data: ", error);
       }
@@ -123,15 +143,34 @@ function FeatureSearch() {
         onTimeChange={handleTimeChange}
       />
       {isLoading && <Loading />}
-      {activities.length > 0 ? (
+      {activities.length > 0 && (
         <Results
           activities={activities}
           class_name={"feature-results-scroll"}
         />
-      ) : (
+      )}
+      {!isLoading && activities.length === 0 && (
         <h1 className="results-container">
           No Results - Try a different query...
         </h1>
+      )}
+      {giveFeedback && (
+        <div className="feedback-container">
+          <FeedbackForm />
+        </div>
+      )}
+      {!isLoading && (
+        <div className="results-container">
+          <button
+            type="button"
+            className="btn btn-primary"
+            onClick={() => {
+              setGiveFeedback(!giveFeedback);
+            }}
+          >
+            Give Feedback
+          </button>
+        </div>
       )}
     </>
   );
